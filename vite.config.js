1336863c1ebb8819b2c19e5c2b9c2f0d8ba1434f
wen <wen.sima@gmail.com>
2024-09-27 20:29:17 +1000

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import react from "@vitejs/plugin-react";
 //import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   // base: "/worldwise/",
   plugins: [react()],
+}); */
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+  if (command !== "serve") {
+    config.base = "/worldwise/";
+  }
+  return config;
 });
