092defb0712e4896127056ebca42e577764b505e
wen <wen.sima@gmail.com>
2024-09-27 17:47:42 +1000

    upate config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/worldwise/",
+  // base: "/worldwise/",
   plugins: [react()],
 });
