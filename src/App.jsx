8a3ea211261b54fbbadd1a7b1f66fcafa6cebfd8
wen <wen.sima@gmail.com>
2024-09-27 17:18:18 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Products";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
