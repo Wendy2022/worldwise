a7687f6fb645f46eb1110c48d52cc042c55a18b3
wen <wen.sima@gmail.com>
2024-09-29 14:51:20 +1000

    add some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,68 @@
-import { HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  HashRouter,
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Products";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
+const BASE_URL = "https://wendy2022.github.io/mock-data/cities.json";
+//const BASE_URL = "http://localhost:9000/cities";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}`);
+        if (!res.ok) {
+          throw new Error("can not get the data");
+        }
+        const data = await res.json();
+        //setCities(data.cities);
+        setCities(data.cities);
+      } catch (error) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Homepage />}></Route>
+          <Route index element={<Homepage />}></Route>
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            ></Route>
+            <Route path="cities/:id" element={<City />} />
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            ></Route>
+            <Route path="form" element={<Form />}></Route>
+          </Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </HashRouter>
